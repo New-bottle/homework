8fd18efbfcf56854bdbffbaff39f6de9f3a63408
闫鸿宇 <775712558@qq.com>
2016-11-25 15:40:28 +0800

    1127 Accepted

--- homework3/1127.cpp
+++ homework3/1127.cpp
@@ -9,21 +9,33 @@ using namespace std;
 const int N = 210;
 
 char s[N];
-int ok[N][N];
-bool judge(int l, int r) 
-{
-	if (ok[l][r] != -1) return ok[l][r];
-	if (l == r) return ok[l][r] = 1;
-	
-	if (s[l] == '{' && s[r] == '}')
-		if (r == l + 1 || judge(l + 1, r - 1)) return ok[l][r] = 1;
-	for(int i = l; i <= r; ++i) {
+int isset[N][N], iselement[N][N], islist[N][N];
+
+bool checklist(int l, int r);
+bool checkset(int l, int r) {
+	if (isset[l][r] != -1) return isset[l][r];
+	int &f = isset[l][r];
+	if (s[l] == '{' && s[r] == '}' && checklist(l + 1, r - 1)) return f = 1;
+	return f = 0;
+}
+bool checkelement(int l, int r) {
+	if (iselement[l][r] != -1) return iselement[l][r];
+	int &f = iselement[l][r];
+	if (l == r) return f = 1;
+	if (checkset(l, r)) return f = 1;
+	return f = 0;
+}
+bool checklist(int l, int r) {
+	if (islist[l][r] != -1) return islist[l][r];
+	int &f = islist[l][r];
+	if (l == r + 1) return f = 1;
+	if (checkelement(l, r)) return f = 1;
+	for(int i = l + 1; i < r; ++i) {
 		if (s[i] == ',') {
-			if (judge(l, i - 1) && judge(i + 1, r)) return ok[l][r] = 1;
-			if (s[l] == '{' && s[r] == '}' && judge(l + 1, i - 1) && judge(i + 1, r - 1)) return ok[l][r] = 1;
+			if (checkelement(l, i - 1) && checklist(i + 1, r)) return f = 1;
 		}
 	}
-	return ok[l][r] = 0;
+	return f = 0;
 }
 
 int main() 
@@ -35,8 +47,10 @@ int main()
 		printf("Word #%d: ", ++ cs);
 		gets(s);
 		int n = strlen(s);
-		memset(ok, -1, sizeof ok);
-		if (judge(0, n - 1)) puts("Set");
+		memset(isset, -1, sizeof isset);
+		memset(iselement, -1, sizeof iselement);
+		memset(islist, -1, sizeof islist);
+		if (checkset(0, n - 1)) puts("Set");
 		else puts("No Set");
 	}
 	return 0;
